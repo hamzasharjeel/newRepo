c3afdceaf011a4fc9f08830264e376803462fd8d
hamzasharjeel <hamzasharjeel8@gmail.com>
2023-12-06 05:32:09 -0800

    pushed

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='mt-24 rounded bg-black/80 py-10 px-6 
+    md:mt-0 md:max-w-sm md:px-14'>
+      <h1>sign up</h1>
+    </div>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='m-6'>
+      <Button>hello world</Button>
+      <h1>hello from the index page</h1>
+    </div>
+  )
+}
+
+
